eab3c3dbe0c57565767e8dae364a75bf0a4754f0
Qudratjon <samidaqudratova@gmail.com>
2025-02-08 21:23:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Luxury Watch Page Loaded");
+});
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Navbar Loaded Successfully");
+});
